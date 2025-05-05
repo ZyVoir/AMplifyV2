93af4cf213a6d15e6a40a4cbfefab232c3dfff94
ZyVoir <whcs.william@gmail.com>
2025-05-05 22:53:32 +0700

    05/05/2025 - Init(Project) - Project Initialization

---
+++ AMplifyV2/Helper/IOHelper.swift
@@ -0,0 +1,8 @@
+//
+//  IOHelper.swift
+//  AMplifyV2
+//
+//  Created by William on 05/05/25.
+//
+
+import Foundation
---
+++ AMplifyV2/View/AMplifyV2App.swift
@@ -0,0 +1,17 @@
+//
+//  AMplifyV2App.swift
+//  AMplifyV2
+//
+//  Created by William on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AMplifyV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+        }
+    }
+}
